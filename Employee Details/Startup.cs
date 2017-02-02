41a2f0fbf1f3c21876263ff6620dc695bfabf446
karthikkumar <karthiksubramaniam@users.noreply.github.com>
2017-02-02 12:55:39 +0530

    Add files via upload

---
+++ Employee Details/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Employee_Details.Startup))]
+namespace Employee_Details
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
